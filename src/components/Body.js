288f6b8e58916ed57f4ef1de6a73c02b2f0848ee
ADITYARAJPUT2505 <ar9412349716@gmail.com>
2024-10-16 23:30:35 +0530

    filter state added for all components

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,14 +2,16 @@ import { ProductCard } from "./Product-card";
 import { useState, useEffect } from "react";
 import Skeleton from "./skeleton";
 export const Body = () => {
-  const [products, setTopProducts] = useState([]);
+  const [products, setProducts] = useState([]);
   const [minRating, setminRating] = useState(0);
-  const [searchText , setSearchText] = useState("");
+  const [searchText, setSearchText] = useState("");
+  const [filterProduct, setfilterProduct] = useState([]);
   const fetchProducts = async () => {
     try {
       const response = await fetch("https://fakestoreapi.com/products");
       const data = await response.json();
-      setTopProducts(data);
+      setProducts(data);
+      setfilterProduct(data);
     } catch (error) {
       console.error("Failed to fetch products", error);
     }
@@ -20,23 +22,28 @@ export const Body = () => {
   }, []);
 
   function handleTopProducts() {
-    setTopProducts(
+    setfilterProduct(
       products.filter((product) => product.rating.rate >= minRating)
     );
   }
 
   function handleSearch() {
-    setTopProducts(
-      products.filter((product) => product.title.toLowerCase().includes(searchText.toLowerCase()))
+    setfilterProduct(
+      products.filter((product) =>
+        product.title.toLowerCase().includes(searchText.toLowerCase())
+      )
     );
   }
 
-  return products.length === 0 ? <Skeleton/> : (
+  return products.length === 0 ? (
+    <Skeleton />
+  ) : (
     <>
       <div>
-
         {/* Input field for the user to enter a minimum rating */}
-        <label htmlFor="ratingInput" className="filter">Enter Minimum Rating: </label>
+        <label htmlFor="ratingInput" className="filter">
+          Enter Minimum Rating:{" "}
+        </label>
         <input
           type="number"
           id="ratingInput"
@@ -52,22 +59,18 @@ export const Body = () => {
         <button onClick={handleTopProducts}>Filter</button>
       </div>
 
-      <div>
-        <label>
-          Search:
-        </label>
+      <div className="search">
+        <label>Search:</label>
         <input
-            type="text"
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-          />
-          <button onClick={handleSearch}>
-            Search
-          </button>
+          type="text"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
       </div>
 
       <div className="Body">
-        {products.map((product) => (
+        {filterProduct.map((product) => (
           <ProductCard key={product.id} products={product} />
         ))}
       </div>
